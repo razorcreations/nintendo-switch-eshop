fa51a8e5d031196fcb48101674898514b4ba9e6a
Jeroen Claassens <support@favware.tech>
2021-11-17 01:15:40 +0100

    style: fix eslint formatting for triple slash directive

--- documentation/src/index.d.ts
+++ documentation/src/index.d.ts
@@ -1,3 +1,3 @@
-// / <reference types="node" />
-// / <reference types="@docusaurus/module-type-aliases" />
-// / <reference types="@docusaurus/theme-classic" />
+/// <reference types="node" />
+/// <reference types="@docusaurus/module-type-aliases" />
+/// <reference types="@docusaurus/theme-classic" />
