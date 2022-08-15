ef3d13ab4f52c559f5d3ccd56b7e70ccc32fa527
Jeroen Claassens <support@favware.tech>
2022-08-15 21:36:28 +0200

    docs: update docusaurus setup

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,5 +1,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const { npm2yarn2pnpm } = require('@sapphire/docusaurus-plugin-npm2yarn2pnpm');
+const { ts2esm2cjs } = require('@sapphire/docusaurus-plugin-ts2esm2cjs');
 
 const Description = 'Unofficial API lib for Nintendo Switch eShop game listing and pricing information.';
 const Email = 'support@favware.tech';
@@ -71,13 +73,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/favna/nintendo-switch-eshop/edit/main/',
-          remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]],
+          remarkPlugins: [npm2yarn2pnpm],
           showLastUpdateAuthor: true,
           showLastUpdateTime: true
         },
         blog: false,
         theme: {
-          customCss: [require.resolve('./src/css/custom.css')]
+          customCss: [require.resolve('./src/css/custom.css'), require.resolve('./src/css/tippy-discord.css')]
         }
       })
     ]
