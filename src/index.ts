9b93ed9a8f7738294f37438420868e9f49c62418
Jeroen Claassens <jeroen.claassens@rws.nl>
2024-04-25 17:06:25 +0200

    feat: drop default export of `getGamesAmerica`
    BREAKING CHANGE: `getGamesAmerica` is no longer default exported. Use a named import instead.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { getGamesAmerica as default, getGamesAmerica } from './lib/getGames/getGamesAmerica';
+export { getGamesAmerica } from './lib/getGames/getGamesAmerica';
 export { getGamesBrazil } from './lib/getGames/getGamesBrazil';
 export { getGamesEurope } from './lib/getGames/getGamesEurope';
 export { getGamesJapan } from './lib/getGames/getGamesJapan';
@@ -13,37 +13,37 @@ export { getShopsByCountryCodes } from './lib/other/getShopByCountryCode';
 export { parseGameCode } from './lib/other/parseGameCode';
 export { parseNSUID } from './lib/other/parseNSUID';
 export {
-  BR_ALGOLIA_HEADERS,
-  BR_ALGOLIA_ID,
-  BR_ALGOLIA_KEY,
-  BR_GET_GAMES_URL,
-  EU_GET_GAMES_URL,
-  JP_GET_GAMES_URL,
-  PRICE_GET_URL,
-  QUERIED_BR_ALGOLIA_KEY,
-  QUERIED_BR_GET_GAMES_URL,
-  QUERIED_US_ALGOLIA_KEY_NEW,
-  QUERIED_US_ALGOLIA_KEY_OLD,
-  QUERIED_US_GET_GAMES_URL_NEW,
-  QUERIED_US_GET_GAMES_URL_OLD,
-  Region,
-  US_ALGOLIA_HEADERS,
-  US_ALGOLIA_ID,
-  US_ALGOLIA_KEY,
-  US_GET_GAMES_URL
+	BR_ALGOLIA_HEADERS,
+	BR_ALGOLIA_ID,
+	BR_ALGOLIA_KEY,
+	BR_GET_GAMES_URL,
+	EU_GET_GAMES_URL,
+	JP_GET_GAMES_URL,
+	PRICE_GET_URL,
+	QUERIED_BR_ALGOLIA_KEY,
+	QUERIED_BR_GET_GAMES_URL,
+	QUERIED_US_ALGOLIA_KEY_NEW,
+	QUERIED_US_ALGOLIA_KEY_OLD,
+	QUERIED_US_GET_GAMES_URL_NEW,
+	QUERIED_US_GET_GAMES_URL_OLD,
+	Region,
+	US_ALGOLIA_HEADERS,
+	US_ALGOLIA_ID,
+	US_ALGOLIA_KEY,
+	US_GET_GAMES_URL
 } from './lib/utils/constants';
 export type {
-  EShop,
-  EURequestOptions,
-  GameEU,
-  GameJP,
-  GameUS,
-  HighlightResult,
-  Nsuid,
-  PriceResponse,
-  QueriedGameResult,
-  QueriedGameUS,
-  QueriedGamesAmericaOptions,
-  TitleData
+	EShop,
+	EURequestOptions,
+	GameEU,
+	GameJP,
+	GameUS,
+	HighlightResult,
+	Nsuid,
+	PriceResponse,
+	QueriedGameResult,
+	QueriedGameUS,
+	QueriedGamesAmericaOptions,
+	TitleData
 } from './lib/utils/interfaces';
 export { EshopError } from './lib/utils/utils';
