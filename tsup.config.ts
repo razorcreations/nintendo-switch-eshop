3d009bc807d142a482be3d21ff79bc156dcebac9
Jeroen Claassens <jeroen.claassens@rws.nl>
2024-04-25 17:36:57 +0200

    feat: ensure proper CJS and ESM bundles and add IIFE bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,37 @@
-import { defineConfig as defineTsupConfig } from 'tsup';
+import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
+import { defineConfig, type Options } from 'tsup';
 
-export default defineTsupConfig({
-  clean: true,
-  dts: true,
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  minify: false,
-  skipNodeModulesBundle: true,
-  sourcemap: true,
-  target: 'es2020',
-  tsconfig: 'src/tsconfig.json',
-  keepNames: true
-});
+const baseOptions: Options = {
+	clean: true,
+	dts: true,
+	entry: ['src/index.ts'],
+	minify: false,
+	sourcemap: true,
+	target: 'es2020',
+	tsconfig: 'src/tsconfig.json',
+	keepNames: true,
+	treeshake: true,
+	esbuildPlugins: [nodeModulesPolyfillPlugin()]
+};
+
+export default [
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/cjs',
+		format: 'cjs',
+		outExtension: () => ({ js: '.cjs' })
+	}),
+	defineConfig({
+		...baseOptions,
+		outDir: 'dist/esm',
+		format: 'esm',
+		outExtension: () => ({ js: '.mjs' })
+	}),
+	defineConfig({
+		...baseOptions,
+		globalName: 'NintendoSwitchEshop',
+		dts: false,
+		outDir: 'dist/iife',
+		format: 'iife'
+	})
+];
